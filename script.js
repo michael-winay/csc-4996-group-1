8e3f71c2e571a1f1e754ee44bea62def2e1698ea
Mike <mgwinay@gmail.com>
2022-07-08 23:35:07 -0400

    Update script.js
    -added email functions to js

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function onClickMenu(){
 	
 	document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+var templateParams = {
+    name: 'James',
+    notes: 'Check this out!'
+};
+ 
+emailjs.send('service_zxu7w58', 'template_ss7e9pp', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+	
+emailjs.sendForm('service_zxu7w58', 'template_ss7e9pp', 'quote')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
