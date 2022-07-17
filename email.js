4547db96f77d6448f79e3af2899e0c4e475e0e0d
Mike <mgwinay@gmail.com>
2022-07-17 10:56:10 -0400

    Updated pages
    -Added email functionality to contact.html
    -Changed webpage titles to be accurate

--- email.js
+++ email.js
@@ -12,6 +12,18 @@ function sendQuoteForm() {
     });
 }
 
+function sendContactForm() {
+	emailjs.sendForm('service_zxu7w58', 'template_u7lixdq', '#contact')
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+	   alert('Message successfully sent! Please allow 3-5 days for a response.');
+	   document.getElementById('quote').reset();
+    }, function(error) {
+       console.log('FAILED...', error);
+	   alert('Message send failure. Please try again later.');
+    });
+}
+
 function sendApplyForm() {
 	emailjs.sendForm('service_zxu7w58', 'apply_template_id_here', '#form ID here')
     .then(function(response) {
